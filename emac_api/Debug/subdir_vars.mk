ff063ee242d1e3bbf1d5d7029da412bda27e3c82
Zachary Farmer <zakkjf@gmail.com>
2018-04-14 16:24:51 -0600

    adding FreeRTOS emac stuff

---
+++ emac_api/Debug/subdir_vars.mk
@@ -0,0 +1,41 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../uart_echo_ccs.cmd 
+
+C_SRCS += \
+../eth.c \
+../startup_ccs.c \
+../uart_echo.c 
+
+C_DEPS += \
+./eth.d \
+./startup_ccs.d \
+./uart_echo.d 
+
+OBJS += \
+./eth.obj \
+./startup_ccs.obj \
+./uart_echo.obj 
+
+OBJS__QUOTED += \
+"eth.obj" \
+"startup_ccs.obj" \
+"uart_echo.obj" 
+
+C_DEPS__QUOTED += \
+"eth.d" \
+"startup_ccs.d" \
+"uart_echo.d" 
+
+C_SRCS__QUOTED += \
+"../eth.c" \
+"../startup_ccs.c" \
+"../uart_echo.c" 
+
+
---
+++ emac_api/Debug/uart_echo.d
@@ -0,0 +1,25 @@
+# FIXED
+
+uart_echo.obj: ../uart_echo.c
+uart_echo.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdint.h
+uart_echo.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdbool.h
+uart_echo.obj: D:/ti/TivaWare/examples/boards/dk-tm4c129x/drivers/pinout.h
+uart_echo.obj: D:/ti/TivaWare/utils/uartstdio.h
+uart_echo.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdarg.h
+uart_echo.obj: D:/ti/TivaWare/driverlib/sysctl.h
+uart_echo.obj: D:/ti/TivaWare/driverlib/debug.h
+uart_echo.obj: D:/ti/TivaWare/driverlib/rom.h
+uart_echo.obj: D:/ti/TivaWare/driverlib/rom_map.h
+uart_echo.obj: ../eth.h
+
+../uart_echo.c: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdint.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdbool.h: 
+D:/ti/TivaWare/examples/boards/dk-tm4c129x/drivers/pinout.h: 
+D:/ti/TivaWare/utils/uartstdio.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdarg.h: 
+D:/ti/TivaWare/driverlib/sysctl.h: 
+D:/ti/TivaWare/driverlib/debug.h: 
+D:/ti/TivaWare/driverlib/rom.h: 
+D:/ti/TivaWare/driverlib/rom_map.h: 
+../eth.h: 
