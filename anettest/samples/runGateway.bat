cb7e8b4e3087c9994178fbe29203af0c7bce1465
Zachary Farmer <zakkjf@gmail.com>
2018-04-17 14:02:51 -0600

    latest

---
+++ anettest/samples/runGateway.bat
@@ -0,0 +1,4 @@
+taskkill /IM anettest.exe
+taskkill /IM anettest.exe
+start anettest -d v1#0 -d v2#1 define sourceIP 88.210.60.143 my_gateway.fws
+start anettest -d v2#0 -d v1#1 define sourceIP 194.85.99.33 my_gateway.fws
