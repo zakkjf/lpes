976a786f8bef63e8cdb02bb3d27035b77fe55a46
Zachary Farmer <zakkjf@gmail.com>
2018-12-06 00:25:47 -0700

    removing uneccessary shit

--- GRADE THIS STUFF/tiva_side/drivers/ubloxneo6.h
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-
-uint8_t detect_gps();
-
-uint8_t init_gps(uint32_t module, uint32_t sysclock);
-
-uint8_t get_gps(uint32_t module, char *msg);
--- sensors_no_tcp/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_H_
-#define MAIN_H_
-
-#define SYSTEM_CLOCK    120000000U
-
-
-#endif /* MAIN_H_ */
